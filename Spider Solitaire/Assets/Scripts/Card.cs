769c4e59d5f0f2d85a2ec0a265892f607fb06632
BoudewijnWitteveen <boudewijn@witteveensoest.nl>
2019-09-17 23:57:25 +0200

    Added Unity Project

---
+++ Spider Solitaire/Assets/Scripts/Card.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Enums;
+
+namespace Game
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField] private CardType m_cardType;
+        [SerializeField] private CardColour m_cardColour;
+
+        [SerializeField] private Sprite m_frontSprite;
+
+        [SerializeField] private bool m_triggerCard;
+
+        private bool m_cardShown;
+
+        #region Properties
+        public Sprite GetFrontSprite => m_frontSprite;
+        public Sprite SetFrontSprite(Sprite frontSprite) => m_frontSprite = frontSprite;
+
+        public bool CardShown => m_cardShown;
+        #endregion
+
+        private void Update()
+        {
+            if(m_triggerCard)
+            {
+                ShowCard(!m_cardShown);
+                m_triggerCard = false;
+            }
+        }
+
+        public void ShowCard(bool cardShownState)
+        {
+            if (cardShownState)
+                transform.localEulerAngles = new Vector3(0, 180, 0);
+            else if(!cardShownState)
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+
+            m_cardShown = cardShownState;
+        }
+    }
+}
---
+++ Spider Solitaire/Assets/Scripts/Enums.cs
@@ -0,0 +1,24 @@
+﻿namespace Game.Enums
+{
+    public enum CardType
+    {
+        cardKing = 0,
+        cardQueen,
+        cardJack,
+        card10,
+        card9,
+        card8,
+        card7,
+        card6,
+        card5,
+        card4,
+        card3,
+        card2,
+        cardA
+    }
+    public enum CardColour
+    {
+        black = 0,
+        red
+    }
+}
